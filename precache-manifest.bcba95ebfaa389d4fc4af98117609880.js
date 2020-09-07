581b57f8642f781519bcdf236ddbf585fb2738a5
akash-akki <akashpakki@gmail.com>
2020-09-07 00:30:04 -0500

    Updates

---
+++ precache-manifest.bcba95ebfaa389d4fc4af98117609880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b8cdd8d810c8433bda499959f69553",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "6944a29b98b10a7502fd",
+    "url": "/resume/static/css/main.6c9dd07a.chunk.css"
+  },
+  {
+    "revision": "a17bf967ac26a8d3feeb",
+    "url": "/resume/static/js/2.5f887cf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.5f887cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6944a29b98b10a7502fd",
+    "url": "/resume/static/js/main.a394bdeb.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  }
+]);
